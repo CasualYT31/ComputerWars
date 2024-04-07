55ec2d153c63de751df965ee837cd3a867cdb846
CasualYT31 <21147925+CasualYT31@users.noreply.github.com>
2024-04-07 04:34:09 +0100

    :construction: Established simpler game properties

--- awe/include/bank-v2.hpp
+++ awe/include/bank-v2.hpp
@@ -38,7 +38,10 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <sstream>
 #include <unordered_set>
 #include "boost/call_traits.hpp"
+#include "SFML/System/Vector2.hpp"
+#include "SFML/System/Time.hpp"
 #include "script.hpp"
+#include "typedef.hpp"
 
 #define GAME_PROPERTY_COUNT 10
 
@@ -94,6 +97,68 @@ namespace std {
 }
 
 namespace awe {
+	/**
+	 * Data used to setup \c awe::random_particles::data objects.
+	 * @sa \c awe::random_particles::data.
+	 */
+	struct particle_data {
+		std::string sheet;
+		std::string spriteID;
+		float density;
+		sf::Vector2f vector;
+		sf::Time respawnDelay;
+	};
+}
+
+namespace awe {
+	template<typename T> struct Serialisable {};
+	template<> struct Serialisable<std::string> {
+		static void fromJSON(std::string& value, engine::json& j,
+			const engine::json::KeySequence& keys) {
+			j.apply(value, keys, true);
+		}
+	};
+	template<> struct Serialisable<sf::Color> {
+		static void fromJSON(sf::Color& value, engine::json& j,
+			const engine::json::KeySequence& keys) {
+			j.applyColour(value, keys, true);
+		}
+	};
+	template<> struct Serialisable<std::vector<particle_data>> {
+		static void fromJSON(std::vector<particle_data>& value, engine::json& j,
+			const engine::json::KeySequence& keys) {
+			nlohmann::ordered_json p;
+			if (j.keysExist({ "particles" }, &p) &&
+				p.is_array() && !p.empty() && p.at(0).is_object()) {
+				value.reserve(p.size());
+				for (const auto& particle : p) {
+					value.emplace_back();
+					if (particle.contains("sheet") &&
+						particle["sheet"].is_string())
+						value.back().sheet = particle["sheet"];
+					if (particle.contains("sprite") &&
+						particle["sprite"].is_string())
+						value.back().spriteID = particle["sprite"];
+					const nlohmann::ordered_json t = static_cast<std::size_t>(0);
+					const nlohmann::ordered_json testFloat = 0.0f;
+					if (particle.contains("density") &&
+						engine::json::equalType(testFloat, particle["density"]))
+						value.back().density = particle["density"];
+					if (particle.contains("vectorx") &&
+						engine::json::equalType(testFloat, particle["vectorx"]))
+						value.back().vector.x = particle["vectorx"];
+					if (particle.contains("vectory") &&
+						engine::json::equalType(testFloat, particle["vectory"]))
+						value.back().vector.y = particle["vectory"];
+					if (particle.contains("respawndelay") &&
+						engine::json::equalType(t, particle["respawndelay"]))
+						value.back().respawnDelay =
+							sf::milliseconds(particle["respawndelay"]);
+				}
+			}
+		}
+	};
+
 	// T = type of field.
 	// N = depth of the hierarchy desired. 1 = just CO, 2 = CO, then weather, etc.
 	//                                     0 = no overrides.
@@ -105,9 +170,9 @@ namespace awe {
 		inline property_field(engine::json& j,
 			const engine::json::KeySequence& keys) {
 			for (auto& i : _scriptNamesWithOverrides) i.insert("");
-			// operator[] automatically creates an empty string for us;
-			// we can always rely on the default value existing.
-			j.apply(_values[{}], keys, true);
+			// We can always rely on the default value existing in a blank state so
+			// long as T is default constructible.
+			awe::Serialisable<T>::fromJSON(_values[{}], j, keys);
 		}
 		inline typename boost::call_traits<T>::reference operator[](
 			const overrides& overrides) {
@@ -140,13 +205,18 @@ namespace awe {
 	// Used to map C++ types to the AS types that are returned from game property
 	// field accessors. "const" is applied automatically by the PROPERTY() macro,
 	// but any references must be added to the AS type if applicable. If a mapping
-	// hasn't been defined here for a given C++ type, and you try to create a field
-	// of that type, the code will fail to compile with an error:
-	//     'value': is not a member of 'awe::AngelScriptType<T>'
-	// where T will be the type you gave.
-	template<typename T> struct AngelScriptType {};
+	// hasn't been defined here for a given C++ type, fields of that type won't be
+	// registered with the script interface. This may be handy in cases where the
+	// data is in a complex format that's not worth adding to the interface
+	// (especially if the scripts won't need that data).
+	template<typename T> struct AngelScriptType {
+		static constexpr char* const value = "";
+	};
 	template<> struct AngelScriptType<std::string> {
 		static constexpr char* const value = "string&";
+	};
+	template<> struct AngelScriptType<sf::Color> {
+		static constexpr char* const value = "Colour&";
 	};
 }
 
@@ -163,6 +233,7 @@ namespace awe {
 public: \
 	n##_(engine::json& j, engine::logger& logger) : _##n(j, { #n }) { e } \
 	static void Register(asIScriptEngine* engine) { \
+		if constexpr (awe::AngelScriptType<ct>::value[0] == '\0') return; \
 		std::stringstream builder; \
 		builder << "const "; \
 		builder << awe::AngelScriptType<ct>::value; \
@@ -186,10 +257,9 @@ public: \
 #define GAME_PROPERTY_DECLARE(cc) class cc { \
 	std::string _scriptName; \
 public: \
-	cc(const std::string& scriptName, engine::json& j, engine::logger& logger) : \
-		_scriptName(scriptName),
+	cc(const std::string& scriptName, engine::json& j, engine::logger& logger) :
 
-#define GAME_PROPERTY_REGISTER(cc, ac, e) { e } \
+#define GAME_PROPERTY_REGISTER(cc, ac, e) _scriptName(scriptName) { e } \
 	static void Register(asIScriptEngine* engine) { \
 		engine->RegisterObjectMethod(ac, "const string& scriptName() const", \
 			asMETHOD(cc, scriptName), asCALL_THISCALL);
@@ -206,17 +276,200 @@ public: \
 	 i: Depth of the hierarchy desired for every field (see awe::property_field).
 	p1: The name of the first field.
 	t1: The C++ type of the first field, without qualifiers.
-	 e: Extra processing that's applied to every field. Can be nothing.
+	e1: Extra processing that's applied to the first field.
+	 e: Extra processing that's applied to every field after every field has been
+	    processed. Can be nothing.
+	 a: Append extra code to the end of the class.
 */
-#define GAME_PROPERTY_1(cc, ac, i, p1, t1, e) \
-	GAME_PROPERTY_DECLARE(cc) \
-        p1(j, logger) \
+
+#define GAME_PROPERTY_1(cc, ac, i, p1, t1, e1, e, a) \
+    GAME_PROPERTY_DECLARE(cc) \
+        p1(j, logger), \
     GAME_PROPERTY_REGISTER(cc, ac, e) \
         p1##_::Register(engine); \
     GAME_PROPERTY_SCRIPTNAME() \
-    PROPERTY(cc, ac, p1, t1, i) \
+    PROPERTY(cc, ac, p1, t1, i, e1) \
+    a \
+};
+
+#define GAME_PROPERTY_4(cc, ac, i, p1, t1, e1, p2, t2, e2, p3, t3, e3, p4, t4, e4, e, a) \
+    GAME_PROPERTY_DECLARE(cc) \
+        p1(j, logger), \
+        p2(j, logger), \
+        p3(j, logger), \
+        p4(j, logger), \
+    GAME_PROPERTY_REGISTER(cc, ac, e) \
+        p1##_::Register(engine); \
+        p2##_::Register(engine); \
+        p3##_::Register(engine); \
+        p4##_::Register(engine); \
+    GAME_PROPERTY_SCRIPTNAME() \
+    PROPERTY(cc, ac, p1, t1, i, e1) \
+    PROPERTY(cc, ac, p2, t2, i, e2) \
+    PROPERTY(cc, ac, p3, t3, i, e3) \
+    PROPERTY(cc, ac, p4, t4, i, e4) \
+    a \
+};
+
+#define GAME_PROPERTY_5(cc, ac, i, p1, t1, e1, p2, t2, e2, p3, t3, e3, p4, t4, e4, p5, t5, e5, e, a) \
+    GAME_PROPERTY_DECLARE(cc) \
+        p1(j, logger), \
+        p2(j, logger), \
+        p3(j, logger), \
+        p4(j, logger), \
+        p5(j, logger), \
+    GAME_PROPERTY_REGISTER(cc, ac, e) \
+        p1##_::Register(engine); \
+        p2##_::Register(engine); \
+        p3##_::Register(engine); \
+        p4##_::Register(engine); \
+        p5##_::Register(engine); \
+    GAME_PROPERTY_SCRIPTNAME() \
+    PROPERTY(cc, ac, p1, t1, i, e1) \
+    PROPERTY(cc, ac, p2, t2, i, e2) \
+    PROPERTY(cc, ac, p3, t3, i, e3) \
+    PROPERTY(cc, ac, p4, t4, i, e4) \
+    PROPERTY(cc, ac, p5, t5, i, e5) \
+    a \
+};
+
+#define GAME_PROPERTY_6(cc, ac, i, p1, t1, e1, p2, t2, e2, p3, t3, e3, p4, t4, e4, p5, t5, e5, p6, t6, e6, e, a) \
+    GAME_PROPERTY_DECLARE(cc) \
+        p1(j, logger), \
+        p2(j, logger), \
+        p3(j, logger), \
+        p4(j, logger), \
+        p5(j, logger), \
+        p6(j, logger), \
+    GAME_PROPERTY_REGISTER(cc, ac, e) \
+        p1##_::Register(engine); \
+        p2##_::Register(engine); \
+        p3##_::Register(engine); \
+        p4##_::Register(engine); \
+        p5##_::Register(engine); \
+        p6##_::Register(engine); \
+    GAME_PROPERTY_SCRIPTNAME() \
+    PROPERTY(cc, ac, p1, t1, i, e1) \
+    PROPERTY(cc, ac, p2, t2, i, e2) \
+    PROPERTY(cc, ac, p3, t3, i, e3) \
+    PROPERTY(cc, ac, p4, t4, i, e4) \
+    PROPERTY(cc, ac, p5, t5, i, e5) \
+    PROPERTY(cc, ac, p6, t6, i, e6) \
+    a \
+};
+
+#define GAME_PROPERTY_7(cc, ac, i, p1, t1, e1, p2, t2, e2, p3, t3, e3, p4, t4, e4, p5, t5, e5, p6, t6, e6, p7, t7, e7, e, a) \
+    GAME_PROPERTY_DECLARE(cc) \
+        p1(j, logger), \
+        p2(j, logger), \
+        p3(j, logger), \
+        p4(j, logger), \
+        p5(j, logger), \
+        p6(j, logger), \
+        p7(j, logger), \
+    GAME_PROPERTY_REGISTER(cc, ac, e) \
+        p1##_::Register(engine); \
+        p2##_::Register(engine); \
+        p3##_::Register(engine); \
+        p4##_::Register(engine); \
+        p5##_::Register(engine); \
+        p6##_::Register(engine); \
+        p7##_::Register(engine); \
+    GAME_PROPERTY_SCRIPTNAME() \
+    PROPERTY(cc, ac, p1, t1, i, e1) \
+    PROPERTY(cc, ac, p2, t2, i, e2) \
+    PROPERTY(cc, ac, p3, t3, i, e3) \
+    PROPERTY(cc, ac, p4, t4, i, e4) \
+    PROPERTY(cc, ac, p5, t5, i, e5) \
+    PROPERTY(cc, ac, p6, t6, i, e6) \
+    PROPERTY(cc, ac, p7, t7, i, e7) \
+    a \
+};
+
+#define GAME_PROPERTY_10(cc, ac, i, p1, t1, e1, p2, t2, e2, p3, t3, e3, p4, t4, e4, p5, t5, e5, p6, t6, e6, p7, t7, e7, p8, t8, e8, p9, t9, e9, p10, t10, e10, e, a) \
+    GAME_PROPERTY_DECLARE(cc) \
+        p1(j, logger), \
+        p2(j, logger), \
+        p3(j, logger), \
+        p4(j, logger), \
+        p5(j, logger), \
+        p6(j, logger), \
+        p7(j, logger), \
+        p8(j, logger), \
+        p9(j, logger), \
+        p10(j, logger), \
+    GAME_PROPERTY_REGISTER(cc, ac, e) \
+        p1##_::Register(engine); \
+        p2##_::Register(engine); \
+        p3##_::Register(engine); \
+        p4##_::Register(engine); \
+        p5##_::Register(engine); \
+        p6##_::Register(engine); \
+        p7##_::Register(engine); \
+        p8##_::Register(engine); \
+        p9##_::Register(engine); \
+        p10##_::Register(engine); \
+    GAME_PROPERTY_SCRIPTNAME() \
+    PROPERTY(cc, ac, p1, t1, i, e1) \
+    PROPERTY(cc, ac, p2, t2, i, e2) \
+    PROPERTY(cc, ac, p3, t3, i, e3) \
+    PROPERTY(cc, ac, p4, t4, i, e4) \
+    PROPERTY(cc, ac, p5, t5, i, e5) \
+    PROPERTY(cc, ac, p6, t6, i, e6) \
+    PROPERTY(cc, ac, p7, t7, i, e7) \
+    PROPERTY(cc, ac, p8, t8, i, e8) \
+    PROPERTY(cc, ac, p9, t9, i, e9) \
+    PROPERTY(cc, ac, p10, t10, i, e10) \
+    a \
 };
 
 namespace awe {
-	GAME_PROPERTY_1(country, "Country", 2, longName, std::string, )
+	GAME_PROPERTY_4(movement_type, "MovementType", 4,
+		longName, std::string,,
+		shortName, std::string,,
+		icon, std::string,,
+		description, std::string,,
+	, )
+
+	GAME_PROPERTY_5(country, "Country", 3,
+		longName, std::string,,
+		shortName, std::string,,
+		icon, std::string,,
+		description, std::string,,
+		colour, sf::Color,,
+		_turnOrder = _turnOrderCounter++;,
+		private: awe::ArmyID _turnOrder; static awe::ArmyID _turnOrderCounter;
+	)
+	awe::ArmyID country::_turnOrderCounter = 0;
+
+	GAME_PROPERTY_6(weather, "Weather", 1,
+		longName, std::string,,
+		shortName, std::string,,
+		icon, std::string,,
+		description, std::string,,
+		sound, std::string,,
+		particles, std::vector<awe::particle_data>,,
+	, )
+
+	GAME_PROPERTY_7(environment, "Environment", 2,
+		longName, std::string,,
+		shortName, std::string,,
+		icon, std::string,,
+		description, std::string,,
+		spritesheet, std::string,,
+		pictureSpritesheet, std::string,,
+		structureIconSpritesheet, std::string,,
+	, )
+}
+
+namespace awe {
+	template<typename T>
+	class bank : public engine::script_registrant, public engine::json_script {
+	public:
+	private:
+		/**
+		 * Each game property is stored in here, accessible via their script names.
+		 */
+		nlohmann::ordered_map<std::string, std::shared_ptr<T>> _bank;
+	};
 }
--- macgen.py
+++ macgen.py
@@ -1,30 +1,40 @@
 USAGE: str = """
-Small script that generates a macro that defines a Game Property Class with the given number of fields.
+Small script that generates macros that define a Game Property Class with the given number of fields.
 Usage: py ./macgen.py 7
 Will generate a macro defining a class with 7 fields, and copy it to the clipboard.
+py ./macgen.py 2 7 10
+Will generate three macros defining classes with 2, 7, and 10 fields respectively, and copy it to the clipboard.
 """
 
 from sys import argv
 from pyperclip import copy
 
 try:
-    FC = int(argv[1])
-    if FC < 1: raise SystemExit
+    for p in argv[1:]:
+        test = int(p)
+        if test < 1: raise SystemExit
 except:
     print(USAGE)
     raise SystemExit
 
-def gen(tmpl: str) -> str:
+def gen(tmpl: str, FC: int) -> str:
     ret: str = ''
     for i in range(1, FC + 1): ret += tmpl.replace('~', f'{i}')
     return ret
 
-copy(f"""
+MACROS: str = ""
 
-#define GAME_PROPERTY_{FC}(cc, ac, i, {gen('p~, t~, ')}e) \\
+for p in argv[1:]:
+    FC = int(p)
+    MACROS += f"""
+
+#define GAME_PROPERTY_{FC}(cc, ac, i, {gen('p~, t~, e~, ', FC)}e, a) \\
     GAME_PROPERTY_DECLARE(cc) \\
-    {gen('    p~(j, logger) \\\n    ')}GAME_PROPERTY_REGISTER(cc, ac, e) \\
-    {gen('    p~##_::Register(engine); \\\n    ')}GAME_PROPERTY_SCRIPTNAME() \\
-{gen('    PROPERTY(cc, ac, p~, t~, i) \\\n')}}};""")
+    {gen('    p~(j, logger), \\\n    ', FC)}GAME_PROPERTY_REGISTER(cc, ac, e) \\
+    {gen('    p~##_::Register(engine); \\\n    ', FC)}GAME_PROPERTY_SCRIPTNAME() \\
+{gen('    PROPERTY(cc, ac, p~, t~, i, e~) \\\n', FC)}    a \\
+}};"""
 
-print("Macro now copied to clipboard!")
+copy(MACROS)
+
+print(f"Macro{'s' if len(argv) > 2 else ''} now copied to clipboard!")
